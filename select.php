81700fcaa13d7a566a16739f354902d413a6de67
Natanael Alves Gabriel <natasnael2002@gmail.com>
2021-11-17 16:06:00 -0300

    changed select return to JSON

--- select.php
+++ select.php
@@ -1,14 +1,20 @@
 <?php
+header("Content-Type: application/json");
 
 $con = new mysqli('localhost', 'root', '', 'exercises');
 $result = mysqli_query($con, 'SELECT * FROM times');
+$results_arr = [];
 
 if ($result){
   while($obj = $result->fetch_object()){
-    $line = '';
-    $line .= $obj->codigo;
-    $line .= "|".$obj->nome;
-    $line .= "|".$obj->estado;
-    echo nl2br($line."\n");
+    $result_obj = new StdClass();
+    $result_obj->codigo = $obj->codigo;
+    $result_obj->nome = $obj->nome;
+    $result_obj->estado = $obj->estado;
+
+    $results_arr[] = $result_obj;
   }
+  echo json_encode($results_arr);
 }
+
+?>
