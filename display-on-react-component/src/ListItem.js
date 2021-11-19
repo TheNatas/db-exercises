d99696b9f0e5719d381e91d612cf1f37292a5900
Natanael Alves Gabriel <natasnael2002@gmail.com>
2021-11-19 14:52:51 -0300

    added react version of displayOnList

--- display-on-react-component/src/ListItem.js
+++ display-on-react-component/src/ListItem.js
@@ -2,26 +2,19 @@ import { useState, useEffect } from 'react';
 
 function ListItem(){
 
-  // const [listItems, setListItems] = useState();
+  const [listItems, setListItems] = useState();
 
-  // useEffect(getRowsFromDatabaseAsList(), listItems)
+  useEffect(() => getRowsFromDatabaseAsList())
   
   const getRowsFromDatabaseAsList = async () => {
     const res = await fetch('http://localhost/exercises/select.php')
     const data = await res.json();
-    console.log(...data.map(row => <li>|{row.codigo}|{row.nome}|{row.estado}</li>));
+    setListItems(data.map(row => <li key={row.codigo}>|{row.codigo}|{row.nome}|{row.estado}</li>));
   };
-
-  useEffect(() => getRowsFromDatabaseAsList())
-
-  // const joinData = async () => {
-  //   const rows = await getRowsFromDatabaseAsList();
-  //   return [...rows];
-  // };
   
   return(
     <>
-      <p>alo</p>
+      {listItems}
     </>
   )
 
