6ff77ac7791a772f4ea1cbbd74852e63f5c9fab1
Natanael Alves Gabriel <natasnael2002@gmail.com>
2021-11-25 17:05:34 -0300

    cleared code

--- displayMatchesByTeamGraphic/script.js
+++ displayMatchesByTeamGraphic/script.js
@@ -1,24 +1,19 @@
 // DATA
 
-let teamsWithNumOfMatches = [];
-
 const fillTeamsWithNumOfMatches = async () => {
   const res = await fetch('../countMatchesByTeam.php');
   const rows = await res.json();
-  teamsWithNumOfMatches = rows;
+  const data = {
+    labels: rows.map(row => row.time),
+    series: [rows.map(row => +row.partidas)]
+  };
+  
+  const options = {
+    width: 1450,
+    height: 700
+  }
+  
+  new Chartist.Bar('.ct-chart', data, options);
 };
 
-fillTeamsWithNumOfMatches()
-  .then(end => {
-    const data = {
-      labels: teamsWithNumOfMatches.map(row => row.time),
-      series: [teamsWithNumOfMatches.map(row => +row.partidas)]
-    };
-    
-    const options = {
-      width: 1450,
-      height: 700
-    }
-    
-    new Chartist.Bar('.ct-chart', data, options);
-  });
+fillTeamsWithNumOfMatches();
