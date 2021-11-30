21b396a74e5c6c46a03c4865c92e79a2a1b444d9
Natanael Alves Gabriel <natasnael2002@gmail.com>
2021-11-30 17:23:19 -0300

    added players table and an option to add them on each team

--- connection.php
+++ connection.php
@@ -1,10 +1,29 @@
 <?php
-header('Location: http://localhost/exercises/displayOnTable/index.html');
 
 $con = new mysqli('localhost', 'root', '', 'exercises');
 
+$data = json_decode(file_get_contents('php://input'));
+
 $prepared_statement = $con->prepare('INSERT INTO times VALUES (NULL, ?, ?, ?)');
-$prepared_statement->bind_param('sss', $_POST['nome'], $_POST['estado'], $_POST['url']);
+$prepared_statement->bind_param('sss', $data->team_name, $data->team_state, $data->team_img);
 $prepared_statement->execute();
 
+foreach($data->players as $player){
+
+  $prepared_statement->close();
+
+  $prepared_statement = $con->prepare('SELECT codigo FROM times WHERE nome = ?');
+  $prepared_statement->bind_param('s', $data->team_name);
+  $prepared_statement->execute();
+  $result = $prepared_statement->get_result();
+  $row = $result->fetch_assoc();
+  
+  $prepared_statement->close();
+
+  $prepared_statement = $con->prepare('INSERT INTO jogadores VALUES (NULL, ?, ?)');
+  $prepared_statement->bind_param('si', $player, $row['codigo']);
+  $prepared_statement->execute();
+
+}
+echo json_encode('http://localhost/exercises/displayOnTable/index.html');
 ?>
--- insertFromForm/script.js
+++ insertFromForm/script.js
@@ -6,8 +6,53 @@ const getStatesFromDatabase = async () => {
     const option = document.createElement('option');
     option.setAttribute('value', state);
     option.textContent = state;
-    document.querySelector('select').appendChild(option);
+    document.querySelector('#estado-select').appendChild(option);
   });
 };
 
+// const getTeamsFromDatabase = async () => {
+//   const res = await fetch('../select.php');
+//   const data = await res.json();
+//   const teams = data.map(row => row.nome);
+//   teams.forEach(team => {
+//     const option = document.createElement('option');
+//     option.setAttribute('value', team);
+//     option.textContent = team;
+//     document.querySelector('#time-select').appendChild(option);
+//   })
+// };
+
+const playerForm = document.querySelector('.add-player-container').innerHTML.slice();
+
+const displayNewPlayerForm = (container) => {
+  container.innerHTML += playerForm;
+};
+
+const displayPlayerForm = function(){
+  const container = document.querySelector('.add-player-container');
+  container.classList.remove('visually-hidden');
+  this.addEventListener('click', () => displayNewPlayerForm(container));
+};
+
+const onSubmitHandler = async function(e){
+  e.preventDefault();
+
+  const res = await fetch('../connection.php', {
+    method: 'POST',
+    body: JSON.stringify({
+      team_name: this.querySelector('#name').value,
+      team_state: this.querySelector('#estado-select').value,
+      team_img: this.querySelector('#img-url-input').value,
+      players: [...this.querySelectorAll('.add-player-container input')]
+        .map(input => input.value)
+    })
+  });
+  const url = await res.json();
+  location.replace(url);
+};
+
 getStatesFromDatabase();
+
+document.querySelector('#btn-add').addEventListener('click', displayPlayerForm, {once: true});
+document.querySelector('.add-player-container input').addEventListener('input', function(e){this.value += e.data});
+document.querySelector('form').addEventListener('submit', onSubmitHandler);
